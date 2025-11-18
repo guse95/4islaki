8787a084eda5aff20766a156db9a760e7065cffd
guse95 <saveliy.gusev.05@list.ru>
2025-11-18 04:49:55 +0300

    Integral

---
+++ Functions_approximation/Integral.cpp
@@ -0,0 +1,90 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+using vd = std::vector<double>;
+
+
+double f(const double x) {
+    return x / ((3 * x + 4) * (3 * x + 4));
+}
+
+double rectangles(const double l, const double r, const double h) {
+    double res = 0;
+    double x_i = l;
+
+    while (x_i + h < r) {
+        res += f((2 * x_i + h) / 2);
+        x_i += h;
+    }
+
+    res += f((x_i + r) / 2);
+    res *= h;
+    return res;
+}
+
+double trapezoids(const double l, const double r, const double h) {
+    double res = f(l) / 2;
+    double x_i = l + h;
+
+    while (x_i < r) {
+        res += f(x_i);
+        x_i += h;
+    }
+
+    res += f(r) / 2;
+    res *= h;
+    return res;
+}
+
+double Simpson(const double l, const double r, const double h) {
+    double res = f(l);
+    double x_i = l + h;
+    int coef = 4;
+
+    while (x_i < r) {
+        res += f(x_i) * coef;
+        x_i += h;
+        coef = (coef == 2) ? 4 : 2;
+    }
+
+    res += f(r);
+    res *= h / 3;
+    return res;
+}
+
+int main() {
+    constexpr double x0 = -1;
+    constexpr double x1 = 1;
+    constexpr double h1 = 0.5;
+    constexpr double h2 = 0.25;
+
+    double F_rec_1 = rectangles(x0, x1, h1);
+    double F_trap_1 = trapezoids(x0, x1, h1);
+    double F_sim_1 = Simpson(x0, x1, h1);
+    std::cout << std::fixed << std::setprecision(5);
+    std::cout << "Padding: " << h1 << std::endl;
+    std:: cout << "Rectangles method: " << F_rec_1 << std::endl;
+    std:: cout << "Trapezoids method: " << F_trap_1 << std::endl;
+    std:: cout << "Simpson`s method: " << F_sim_1 << std::endl;
+
+    double F_rec_2 = rectangles(x0, x1, h2);
+    double F_trap_2 = trapezoids(x0, x1, h2);
+    double F_sim_2 = Simpson(x0, x1, h2);
+    std::cout << "Padding: " << h2 << std::endl;
+    std:: cout << "Rectangles method: " << F_rec_2 << std::endl;
+    std:: cout << "Trapezoids method: " << F_trap_2 << std::endl;
+    std:: cout << "Simpson`s method: " << F_sim_2 << std::endl;
+
+    std::cout << "Runge-Romberg-Richardson integral:" << std::endl;
+    double k = h1 / h2;
+    double F_rec_specified = F_rec_2 + (F_rec_2 - F_rec_1) /
+        (k * k - 1);
+    double F_trap_specified = F_trap_2 + (F_trap_2 - F_trap_1) /
+        (k * k - 1);
+    double F_sim_specified = F_sim_2 + (F_sim_2 - F_sim_1) /
+        (k * k * k * k - 1);
+    std:: cout << "Rectangles method: " << F_rec_specified << std::endl;
+    std:: cout << "Trapezoids method: " << F_trap_specified << std::endl;
+    std:: cout << "Simpson`s method: " << F_sim_specified << std::endl;
+}
